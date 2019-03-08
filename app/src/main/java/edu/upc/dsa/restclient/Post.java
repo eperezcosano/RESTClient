ba381d807a9b22c68c6c2316ea615443eba96c24
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-08 20:56:47 +0100

    added Post class

---
+++ app/src/main/java/edu/upc/dsa/restclient/Post.java
@@ -0,0 +1,15 @@
+package edu.upc.dsa.restclient;
+
+public class Post {
+
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
